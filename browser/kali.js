9a4efb7222ac8a68c2cd48b4ee9f7a00b3013f73
g0tmi1k <g0tmi1k@kali.org>
2019-05-05 22:27:11 +0100

    Remove Firefox Update page

--- browser/kali.js
+++ browser/kali.js
@@ -1,3 +1,7 @@
 /* Kali default settings */
+/* Homepage */
 pref("browser.startup.homepage", "data:text/plain,browser.startup.homepage=file:///usr/share/kali-defaults/web/homepage.html");
+/* Bookmarks */
 pref("browser.showPersonalToolbar", true);
+/* Disable "Firefox Update" page */
+pref("browser.startup.homepage_override.mstone", "ignore");
