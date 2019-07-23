4bcdc7be2f1d1d795db13edf393df84bd3c3bf52
Raphaël Hertzog <raphael@offensive-security.com>
2019-07-23 12:27:08 +0200

    Move the remaining preferences into policies.json

--- browser/kali.js
+++ browser/kali.js
@@ -1,7 +1,2 @@
 /* Kali default settings */
-/* Homepage */
-pref("browser.startup.homepage", "data:text/plain,browser.startup.homepage=file:///usr/share/kali-defaults/web/homepage.html");
-/* Bookmarks */
-pref("browser.showPersonalToolbar", true);
-/* Disable "Firefox Update" page */
-pref("browser.startup.homepage_override.mstone", "ignore");
+/* Everything is handled through policies.json */
